396978a43b089e5c0bbe82ae7aa6b675c47cb2e1
andrew <andrewmuhn@gmail.com>
2023-05-04 14:44:12 -0700

    feat: got recipe post route working, tweaked get

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -41,10 +41,12 @@ router.get('/:id', async (req, res) => {
         {
           model: Ingredient,
           through: RecipeIngredient,
+          // attributes: ['unit', 'amount'],
           as: 'recipe_ingredients',
         },
         {
           model: Step,
+          attributes: ['step'],
         },
       ],
     });
@@ -62,14 +64,24 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// got this from the module 13 challenge
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   // {
-  //   name: "Lasagna",
+  //   name: 'Lasagna',
   //   user_id: 3,
-  //   ingredients: [mozzerella cheese, lasagna noodles],
-  //   steps: ['step 1 is', 'step 2 ...', 'step 3']
-  // }
+  //   ingredients: [
+  //     {
+  //       name: 'mozzerella cheese',
+  //       unit: 'cup',
+  //       amount: 2,
+  //     },
+  //     {
+  //       name: 'lasagna noodles',
+  //       unit: 'oz',
+  //       amount: 16,
+  //     },
+  //   ],
+  //   steps: ['step 1 is', 'step 2 ...', 'step 3'],
+  // };
   if (!req.body.name) {
     res
       .status(400)
@@ -78,21 +90,26 @@ router.post('/', async (req, res) => {
   }
 
   try {
+    // * Create Recipe
     const recipe = await Recipe.create(req.body);
-    const existingIngredients = await Ingredient.findAll();
-    const existingIngredientNames = existingIngredients.map(
-      (ingredient) => ingredient.dataValues.name.trim().toLowerCase()
-    );
-    console.log(existingIngredientNames);
-    console.log(req.body.ingredients);
-    const newIngredients = [];
+
+    // if there are ingredients create them and the relation to the recipe via recipe_ingredient
     if (req.body.ingredients.length) {
+      // Find all existing ingredients and their names.
+      const existingIngredients = await Ingredient.findAll();
+      const existingIngredientNames = existingIngredients.map(
+        (ingredient) =>
+          ingredient.dataValues.name.trim().toLowerCase()
+      );
+      // * Create Ingredients that didn't already exist
+      const newIngredients = [];
+      // checks if the ingredient from req.body already exists in the DB. if it doesn't then it adds it to the arr newIngredients to be created.
       req.body.ingredients.forEach(async (ingredient) => {
-        if (!existingIngredientNames.includes(ingredient)) {
-          newIngredients.push(ingredient);
+        if (!existingIngredientNames.includes(ingredient.name)) {
+          newIngredients.push(ingredient.name);
         }
       });
-      console.log(newIngredients);
+      // takes the newIngredients and adds the extra attrtibute, category
       const newIngredientsArr = newIngredients.map((name) => {
         return {
           name: name,
@@ -103,17 +120,48 @@ router.post('/', async (req, res) => {
         newIngredientsArr
       );
 
-      // const recipeIngredientIdArr = req.body.ingredient.map(
-      //   (ingredient_id) => {
-      //     return {
-      //       recipe_id: recipe.id,
-      //       ingredient_id,
-      //     };
-      //   }
-      // );
-      // return RecipeIngredient.bulkCreate(recipeIngredientIdArr);
-      res.status(204);
+      // * Create Recipe Ingredients relationships
+      const allIngredients =
+        existingIngredients.concat(createIngredients);
+
+      // for each ingredient in the req.body find it's id from allIngredients then return the recipe_id as the id of the recipe created earlier the ingredient_id as the ingredient.id just found and the extra data atttributes for the req.body
+      const recipeIngredientsArr = req.body.ingredients.map(
+        (ingredient) => {
+          let ingredientId;
+          for (let i = 0; i < allIngredients.length; i++) {
+            const ingredientObj = allIngredients[i];
+            if (
+              ingredientObj.dataValues.name.trim().toLowerCase() ===
+              ingredient.name.trim().toLowerCase()
+            ) {
+              ingredientId = ingredientObj.dataValues.id;
+            }
+          }
+          return {
+            recipe_id: recipe.id,
+            ingredient_id: ingredientId,
+            unit: ingredient.unit,
+            amount: ingredient.amount,
+          };
+        }
+      );
+
+      const createRecipeIngredients =
+        await RecipeIngredient.bulkCreate(recipeIngredientsArr);
     }
+
+    // If there are steps create them
+    if (req.body.steps.length) {
+      console.log(req.body.steps);
+      const recipeStepsArr = req.body.steps.map((step) => {
+        return {
+          recipe_id: recipe.id,
+          step: step,
+        };
+      });
+      const createRecipeSteps = await Step.bulkCreate(recipeStepsArr);
+    }
+    res.status(200).json({ message: 'Recipe Created!' });
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
