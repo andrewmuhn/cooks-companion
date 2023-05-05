905aef72245fa1e46906967d034e1e289f77c618
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-05-04 20:02:56 -0700

    homeroute for recipe input added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -76,7 +76,27 @@ router.get('/recipe/:id', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get('/recipe_input', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [
+        {
+          model: Recipe,
+        },
+      ],
+    });
 
+    const user = userData.get({ plain: true });
+    console.log(user);
+    res.render('recipeinput', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/profile');
