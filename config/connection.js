4fcf058d7070b366bee977304868d4b5fb755661
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-05-06 12:02:34 -0700

    port added

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,18 @@ require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-  });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        dialectOptions: {
+          decimalNumbers: true,
+          port: 3306,
+        },
+      }
+    );
 
 module.exports = sequelize;
